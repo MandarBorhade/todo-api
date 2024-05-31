9b78c423b2401382c8ca8ab181ec62fedab6a307
MandarBorhade <mandarva.borade@gmail.com>
2024-05-31 19:21:53 +0530

    feature: login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -50,9 +50,42 @@ const userRegister = async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const userLogin = async (req, res) => {
-  return res.status(200).json({
-    message: "User logged in",
-  });
+  // accept username and email
+  const { username, password } = req.body;
+  // compare password passed with hashed password from DB
+  if (!username || !password) {
+    return res.status(400).json({
+      error: "All fields are mandatory",
+    });
+  }
+
+  const user = await User.findOne({ username });
+
+  const comparePassword = await bcrypt.compare(password, user.password);
+
+  if (user && comparePassword) {
+    // Generate accessToken
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({
+      message: "User logged in",
+      data: accessToken,
+    });
+  } else {
+    res.status(401).json({
+      error: "Username or password invalid",
+    });
+  }
 };
 
 module.exports = {
